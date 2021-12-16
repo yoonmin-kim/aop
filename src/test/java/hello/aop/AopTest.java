49f57e956afacaaef370312d64076e78550e7ab3
yoonmin-kim <rladbsals23@gmail.com>
2021-12-16 17:55:31 +0900

    feat: 포인트컷 분리
    @Around 에 포인트컷을 직접 작성할 수 있지만
    이미 작성한 포인트컷을 다른 어드바이저에서도 재사용 할 수있다
    
    AspectV2 의 코드를 보면 알 수 있는데 @Pointcut 을 적용한
    메소드를 작성해두고 @Around 에 해당 메소드를 작성한다

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -7,13 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
+// @Import(AspectV1.class)
+@Import(AspectV2.class)
 public class AopTest {
 
 	@Autowired
