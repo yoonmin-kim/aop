ed7cfd4fa4527a62e82d0d3330dabd213a4bd971
yoonmin-kim <rladbsals23@gmail.com>
2021-12-17 16:01:06 +0900

    feat: 어드바이스 추가
    포인트컷을 추가한뒤 @Around 에 조건을 추가하였다.
    
    // hello.aop.order 패키지와 하위 패키지 이면서 클래스 이름 패턴이 *Service
    @Around("allOrder() && allService()")
    
    -> allOrder() 에서는 모든 반환 타입이면서 hello.aop.order 패키지와 하위 패키지를 포인트컷 조건으로 가지고있고
       allService() 는 모든 반환 타입이면서 모든 패키지이고 클래스이름 패턴이 *Service인 모든 메소드를 조건으로 가지고있다
       따라서 위 두조건을 만족하는 메소드만 해당 어드바이스가 적용된다.

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,7 +2,6 @@ package hello.aop;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +10,15 @@ import org.springframework.context.annotation.Import;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
 // @Import(AspectV1.class)
-@Import(AspectV2.class)
+// @Import(AspectV2.class)
+@Import(AspectV3.class)
 public class AopTest {
 
 	@Autowired
