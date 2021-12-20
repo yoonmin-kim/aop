a523520f6bae53b7e0c761c9f7cb17e5c814e632
yoonmin-kim <rladbsals23@gmail.com>
2021-12-20 22:54:55 +0900

    feat: 어드바이스 종류
    @Around 는 사실 다음의 어드바이스들을 모두 포함하고 있다.
    
    @Before
    - ProceedingJoinPoint 의 proceed()가 호출되기 전을 담당한다
    - ProceedingJoinPoint 의 proceed()는 자동으로 호출된다
    
    @AfterReturning
    - ProceedingJoinPoint 의 proceed()가 호출된 직후를 담당한다
    - returning 옵션과 파라미터명을 맞춰주면 proceed()의 결과값을
      받을 수 있다
    - 파라미터 타입은 결과값의 타입과 같거나 상위 타입이여야 한다
    
    @AfterThrowing
    - proceed() 호출도중 발생된 오류를 catch 할 수 있다
    - throwing 옵션과 파라미터명을 맞춰주면 된다
    - 파라미터 타입은 발생된 Exception 과 타입이 같거나 상위 타입이여야한다
    
    @After
    - 자원 반환의 역할을 담당한다, finally 구문
    
    [@Around 만 쓰지않고 세분화된 어드바이스가 존재하는 이유는?]
    만약, @Around 에서 실수로 proceed() 를 호출하지 않으면 큰 장애가 발생할 수 있다
    @Before 를 사용하면 자동으로 호출해주기 때문에 실수가 발생할 일이 없다
    
    또한, 어노테이션이 명확히 해당 어드바이스의 역할을 알려준다 단순히 @Before의 기능으로
    @Around 를 사용할 경우 해당 코드를 처음 보는 사람이라면 단번에 @Before의 목적으로
    사용되었겠구나 하고 이해하기 힘들다

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -14,6 +14,7 @@ import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
 import hello.aop.order.aop.AspectV4Pointcut;
 import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -22,7 +23,8 @@ import lombok.extern.slf4j.Slf4j;
 // @Import(AspectV2.class)
 // @Import(AspectV3.class)
 // @Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+// @Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 
 	@Autowired
