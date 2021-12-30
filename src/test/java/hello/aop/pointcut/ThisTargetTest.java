e2a6c5b1d02528c3430ac7b34789d7afd8107e3d
yoonmin-kim <rladbsals23@gmail.com>
2021-12-30 21:34:55 +0900

    feat: this, target
    this든 target이든 스프링부트는 기본적으로 CGLIB를
    사용하기 때문에 문제가 안된다
    
    하지만 JDK 동적프록시를 적용할때
    프록시 객체는 target의 인터페이스를 가지고 프록시를
    생성하기 때문에 this-impl이 동작하지 않게된다.

---
+++ src/test/java/hello/aop/pointcut/ThisTargetTest.java
@@ -0,0 +1,58 @@
+package hello.aop.pointcut;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import hello.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+/*
+ * spring.aop.proxy-target-class=true CGLIB
+ * spring.aop.proxy-target-class=false JDK 동적 프록시
+ */
+@Slf4j
+@Import(ThisTargetTest.ThisTargetAspect.class)
+@SpringBootTest(properties = "spring.aop.proxy-target-class=false")
+public class ThisTargetTest {
+
+	@Autowired
+	MemberService memberService;
+
+	@Test
+	void success() {
+		log.info("memberService = {}", memberService.getClass());
+		memberService.hello("helloA");
+	}
+
+	@Slf4j
+	@Aspect
+	static class ThisTargetAspect {
+		@Around("this(hello.aop.member.MemberService)")
+		public Object dothisInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+			log.info("[this-interface] {}", joinPoint.getSignature());
+			return joinPoint.proceed();
+		}
+
+		@Around("target(hello.aop.member.MemberService)")
+		public Object dotargetInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+			log.info("[target-interface] {}", joinPoint.getSignature());
+			return joinPoint.proceed();
+		}
+
+		@Around("this(hello.aop.member.MemberServiceImpl)")
+		public Object doThis(ProceedingJoinPoint joinPoint) throws Throwable {
+			log.info("[this-impl] {}", joinPoint.getSignature());
+			return joinPoint.proceed();
+		}
+
+		@Around("target(hello.aop.member.MemberServiceImpl)")
+		public Object doTarget(ProceedingJoinPoint joinPoint) throws Throwable {
+			log.info("[target-impl] {}", joinPoint.getSignature());
+			return joinPoint.proceed();
+		}
+	}
+}
